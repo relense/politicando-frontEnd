a51f3d2a12ae4cf9087c80fd857121937f7c8e26
Miguel Furtado <miguelmofurtado@gmail.com>
2018-10-13 16:36:39 +0100

    changed how the list of parties behaves

--- src/components/list/List.js
+++ src/components/list/List.js
@@ -6,6 +6,14 @@ import './List.css';
 
 
 class List extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      open: false,
+      index: null
+    };
+  }
 
   componentWillMount() {
     this.props.fetchParties()
@@ -14,6 +22,11 @@ class List extends React.Component {
   selectPartie = (i) => {
     this.props.changeView("Partie");
     this.props.changeCurrentPartie(this.props.parties[i]);
+
+    this.setState({
+      open: this.state.open ? ((this.state.index === i) ? false : true) : true,
+      index: i
+    })
   }
 
   renderData = () => {
@@ -34,10 +47,51 @@ class List extends React.Component {
     return data;
   }
 
+  renderOpenData = (j) => {
+    let data = [];
+
+    if (this.props.parties.length !== 0) {
+      for(let i = 0; i < this.props.parties.length; i++) {
+
+        let title = this.props.parties[i].description + ": " + this.props.parties[i].deputies_republic_chamber + " - Assembleia da República";
+        if(j === i) {
+          data.push(
+              <div key={i} className="partieNameContainerSelected" title={title}>
+                <div className="partieTitle" onClick={() => this.selectPartie(i)}>
+                  { this.props.parties[i].party_name } - {this.props.parties[i].deputies_republic_chamber}
+                </div>
+                <div className="partieChildContainer">
+                  <div className="partieItem">Basic Information</div>
+                  <div className="partieItem">Seats in Government</div>
+                  <div className="partieItem">News</div>
+                </div>
+              </div>
+          );
+        } else {
+          data.push(
+              <div key={i} className="partieNameContainer" title={title} onClick={() => this.selectPartie(i)}>
+                  { this.props.parties[i].party_name } - {this.props.parties[i].deputies_republic_chamber}
+              </div>
+          );
+        }
+      }
+    }
+
+    return data;
+  }
+
+  renderContent = () => {
+    if(!this.state.open) {
+      return this.renderData();
+    } else {
+      return this.renderOpenData(this.state.index);
+    }
+  }
+
   render() {
     return (
       <div className="mainContainter">
-        { this.renderData() }
+        { this.renderContent() }
       </div>
     );
   }
