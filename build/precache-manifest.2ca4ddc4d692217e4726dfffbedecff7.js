672c16b7f80b57137558327d3d37af71368d96f3
Miguel Mendes de Oliveira Furtado <miguelmofurtado@gmail.com>
2019-01-15 14:24:54 +0000

    build folder

---
+++ build/precache-manifest.2ca4ddc4d692217e4726dfffbedecff7.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = [
+  {
+    "revision": "f66b839d1a06cac31b4426aea58f5a1b",
+    "url": "/static/media/nc_logo.f66b839d.png"
+  },
+  {
+    "revision": "341d2878a5fcd7a26290",
+    "url": "/static/css/main.696b5531.chunk.css"
+  },
+  {
+    "revision": "b20dba8e6f08ef787e8cfb42b64b620e",
+    "url": "/static/media/ptp_logo.b20dba8e.png"
+  },
+  {
+    "revision": "0248b5405973d9eeae2e",
+    "url": "/static/js/1.0248b540.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "f402429dbfecc59f9c5eb5b8908c41e5",
+    "url": "/static/media/be_logo.f402429d.png"
+  },
+  {
+    "revision": "7094902b23d1bd4f0301f02af5923549",
+    "url": "/static/media/cds_logo.7094902b.png"
+  },
+  {
+    "revision": "6e6fca9a94864524e3bf917a0c860c98",
+    "url": "/static/media/jpp_logo.6e6fca9a.png"
+  },
+  {
+    "revision": "84ea79dd9e6729e63e1b319175782a89",
+    "url": "/static/media/mpt_logo.84ea79dd.png"
+  },
+  {
+    "revision": "341d2878a5fcd7a26290",
+    "url": "/static/js/main.341d2878.chunk.js"
+  },
+  {
+    "revision": "29cb2799376dee7bf32092afe7e75422",
+    "url": "/static/media/pan_logo.29cb2799.png"
+  },
+  {
+    "revision": "efbbd6a70543a192ebc2eeabb6ad3fab",
+    "url": "/static/media/pcp_logo.efbbd6a7.png"
+  },
+  {
+    "revision": "c31459496bc42d00575412b045125008",
+    "url": "/static/media/pdr_logo.c3145949.png"
+  },
+  {
+    "revision": "ab1cbe669bd854ab82b3220e7efa34c0",
+    "url": "/static/media/pev_logo.ab1cbe66.png"
+  },
+  {
+    "revision": "75758b10a94ac40d9fe888176a7e9c1c",
+    "url": "/static/media/ppm_logo.75758b10.png"
+  },
+  {
+    "revision": "71115ff24487ef530cbb2293893d9b95",
+    "url": "/static/media/ps_logo.71115ff2.png"
+  },
+  {
+    "revision": "0248b5405973d9eeae2e",
+    "url": "/static/css/1.5383fa94.chunk.css"
+  },
+  {
+    "revision": "7ece416bdb07a7135be9be658354f893",
+    "url": "/index.html"
+  }
+];
