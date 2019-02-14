6730d759d963aec335146abcce7d95ece21daecf
miguelmofurtado <miguelmofurtado@gmail.com>
2019-02-14 21:21:47 +0000

    no message

---
+++ build/precache-manifest.39ba11915f85f0ff9c480f49415e6925.js
@@ -0,0 +1,114 @@
+self.__precacheManifest = [
+  {
+    "revision": "971d086db6d7dbec8a20",
+    "url": "/static/css/main.14673d40.chunk.css"
+  },
+  {
+    "revision": "971d086db6d7dbec8a20",
+    "url": "/static/js/main.971d086d.chunk.js"
+  },
+  {
+    "revision": "847836dee81812c34c9f",
+    "url": "/static/css/1.7d574cae.chunk.css"
+  },
+  {
+    "revision": "847836dee81812c34c9f",
+    "url": "/static/js/1.847836de.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "636d4b4069b23b703c61065d88cb464f",
+    "url": "/static/media/alianca_logo.636d4b40.png"
+  },
+  {
+    "revision": "f402429dbfecc59f9c5eb5b8908c41e5",
+    "url": "/static/media/be_logo.f402429d.png"
+  },
+  {
+    "revision": "7094902b23d1bd4f0301f02af5923549",
+    "url": "/static/media/cds_logo.7094902b.png"
+  },
+  {
+    "revision": "bd11f52637854d613c4a62cee67e60c6",
+    "url": "/static/media/il_logo.bd11f526.png"
+  },
+  {
+    "revision": "6e6fca9a94864524e3bf917a0c860c98",
+    "url": "/static/media/jpp_logo.6e6fca9a.png"
+  },
+  {
+    "revision": "58e5762896f38678d89edc8d709e2500",
+    "url": "/static/media/l_logo.58e57628.png"
+  },
+  {
+    "revision": "37423907a8d324f46c2b36b9875b0ba2",
+    "url": "/static/media/livre_logo_v2.37423907.png"
+  },
+  {
+    "revision": "2abb7778b3aabab23d6409ef44d74b38",
+    "url": "/static/media/mas_logo.2abb7778.png"
+  },
+  {
+    "revision": "84ea79dd9e6729e63e1b319175782a89",
+    "url": "/static/media/mpt_logo.84ea79dd.png"
+  },
+  {
+    "revision": "f66b839d1a06cac31b4426aea58f5a1b",
+    "url": "/static/media/nc_logo.f66b839d.png"
+  },
+  {
+    "revision": "29cb2799376dee7bf32092afe7e75422",
+    "url": "/static/media/pan_logo.29cb2799.png"
+  },
+  {
+    "revision": "efbbd6a70543a192ebc2eeabb6ad3fab",
+    "url": "/static/media/pcp_logo.efbbd6a7.png"
+  },
+  {
+    "revision": "1e66c65b6b796fcfc9f36d536b091124",
+    "url": "/static/media/pctp_logo.1e66c65b.png"
+  },
+  {
+    "revision": "c31459496bc42d00575412b045125008",
+    "url": "/static/media/pdr_logo.c3145949.png"
+  },
+  {
+    "revision": "ab1cbe669bd854ab82b3220e7efa34c0",
+    "url": "/static/media/pev_logo.ab1cbe66.png"
+  },
+  {
+    "revision": "97e15ae86e0484ba5a63b9089128714b",
+    "url": "/static/media/pld_logo.97e15ae8.png"
+  },
+  {
+    "revision": "f7eb70a4b96b7284394033f4b70aa8f5",
+    "url": "/static/media/pnr_logo.f7eb70a4.png"
+  },
+  {
+    "revision": "75758b10a94ac40d9fe888176a7e9c1c",
+    "url": "/static/media/ppm_logo.75758b10.png"
+  },
+  {
+    "revision": "5353606a31315cd80019c1818419d7f0",
+    "url": "/static/media/ppv_cdc_logo.5353606a.png"
+  },
+  {
+    "revision": "71115ff24487ef530cbb2293893d9b95",
+    "url": "/static/media/ps_logo.71115ff2.png"
+  },
+  {
+    "revision": "b20dba8e6f08ef787e8cfb42b64b620e",
+    "url": "/static/media/ptp_logo.b20dba8e.png"
+  },
+  {
+    "revision": "b7469a54a1a0a661c7842cbeb9544165",
+    "url": "/static/media/purp_logo.b7469a54.png"
+  },
+  {
+    "revision": "4a2ba6bcd17faaf6a8a2e4d8866fabf1",
+    "url": "/index.html"
+  }
+];
